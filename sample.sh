37a73d2de6c0bb075bd9450762516b3347cd5b1d
kcvamsi <kcvamsi98@gmail.com>
2023-04-18 21:19:35 +0530

    echo

--- sample.sh
+++ sample.sh
@@ -1,2 +1,19 @@
 echo hello world
 
+# how to enable color
+
+#syntax = echo -e "\e[COLmMESSAGE\e[0m"
+
+# -e is used to enable the 'e' which is enable color in echo cmnd
+
+#all the inputs should be there in double quotes.
+
+#COLm - is the color code, we have red(31m), green(32m), yellow(33m), blue(34m), magenta(35m), cyan(36m)
+
+#\e[0m - is used to disable the color, because once we enable we need to disable otherwise it will follow to next.
+
+#so the syntax is = echo -e "\e[31mvamsi\e[0m"
+
+#example
+
+echo -e "\e[31mkcvamsi\e[0m"
