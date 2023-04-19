1402946fd4e04bd5aa170a0aa68064946c24cc4c
kcvamsi <kcvamsi98@gmail.com>
2023-04-19 20:58:55 +0530

    ar

--- variables.sh
+++ variables.sh
@@ -4,7 +4,10 @@ a=1
 name=kc
 echo a = $a
 echo name = $name
+OR WE CAN USE 
+echo name = ${name}
 
+# {} are not always important but for some places they are needed like arthematic operations
 
 # for dynamic things which changes daily like date
 
