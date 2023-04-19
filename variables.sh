37425eb1c918b68136ce8b4c85ed8db5c4d07c67
kcvamsi <kcvamsi98@gmail.com>
2023-04-19 20:50:03 +0530

    ART

--- variables.sh
+++ variables.sh
@@ -5,5 +5,14 @@ name=kc
 echo a = $a
 echo name = $name
 
+
+# for dynamic things which changes daily like date
+
 DATE=$(date +%F)
 echo today date = $DATE
+
+
+# for arthematic expressions
+
+ARTH=$((2+10-3/3))
+echo ARTH = $(ARTH)
